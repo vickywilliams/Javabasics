e156be1b0fe532c3ec46f9ba6e2a7b8cf07949a3
vJ@720026 <vickywilliams@gmail.com>
2024-02-02 03:29:28 -0800

    i have land java

---
+++ src/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int i = 235;
+        int value = i % 2;
+        switch (value) {
+            case 1:
+                System.out.println("the value odd");
+                break;
+            case 0:
+                System.out.println("the value even");
+                break;
+        }
+    }
+}
